36d36eaddf9c6db9275e41987f6e1c12d8ebbbdb
sryung <sryung1225@gmail.com>
2023-01-06 15:56:18 +0900

    [Day29] click event 학습

--- core_javascript/client/chapter/dom/01.traversal.js
+++ core_javascript/client/chapter/dom/01.traversal.js
@@ -29,7 +29,7 @@
 
 // let first = document.querySelector(".first");
 
-let [first, second] = document.querySelectorAll("span");
+// let [first, second] = document.querySelectorAll("span");
 console.log(getNode(".first"));
 
 
@@ -41,3 +41,16 @@ console.log(getNode(".first").matches(".first"));
 // - contains
 // 선택자의 자식들 중에 해당 element가 있어?
 console.log(getNode("h1").contains(getNode(".first")));
+
+
+
+let first = getNode('.first');
+let clicked = false;
+document.addEventListener('click',()=>{
+  if(first.classList.contains('first') && !clicked){
+    first.classList.add('is-active')
+  }else{
+    first.classList.remove('is-active')
+  }
+  clicked = !clicked;
+})
