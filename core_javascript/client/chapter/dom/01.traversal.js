77c0f83860d8b7e42f6e66d2c07f88024976e6fa
sryung <sryung1225@gmail.com>
2023-01-06 15:53:41 +0900

    [Day29] getNode 생성

---
+++ core_javascript/client/chapter/dom/01.traversal.js
@@ -0,0 +1,43 @@
+/* ---------------------------------------------------------------------- */
+/* DOM traversal                                                          */
+/* ---------------------------------------------------------------------- */
+
+
+/* 모든 노드에서 사용 => 공백도 인식함 */
+// - parentNode
+// - childNodes
+// - firstChild
+// - lastChild
+// - previousSibling
+// - nextSibling
+
+/* 요소 노드에서만 사용 가능 */
+// - parentElement
+// - children
+// - firstElementChild
+// - lastElementChild
+// - previousElementSibling
+// - nextElementSibling
+
+/* 문서 대상 찾기 */
+// - getElementById
+// - getElementsByTagName
+// - getElementsByClassName
+// - querySelector
+// - querySelectorAll
+// - closest
+
+// let first = document.querySelector(".first");
+
+let [first, second] = document.querySelectorAll("span");
+console.log(getNode(".first"));
+
+
+/* 문서 대상 확인 */
+// - matches
+// 선택자 안에 class/id 를 갖고 있는 대상이 있어?
+console.log(getNode(".first").matches(".first"));
+
+// - contains
+// 선택자의 자식들 중에 해당 element가 있어?
+console.log(getNode("h1").contains(getNode(".first")));
