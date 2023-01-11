b00e1e6bf076579aa9a8302d93ba36b279a2012e
sryung <sryung1225@gmail.com>
2023-01-11 10:35:13 +0900

    [Day31] styling 학습 - 유틸함수 css.js 만들기

---
+++ core_javascript/client/chapter/dom/05.styling.js
@@ -0,0 +1,50 @@
+/* ---------------------------------------------------------------------- */
+/* DOM Styling                                                            */
+/* ---------------------------------------------------------------------- */
+
+const first = getNode(".first");
+
+/* 클래스를 관리할 수 있게 해주는 DOM 프로퍼티 ------------------------------------ */
+
+// - className – 클래스 전체를 문자열 형태로 반환해주는 프로퍼티로 클래스 전체를 관리할 때 유용
+// - classList – 클래스 하나를 관리할 수 있게 해주는 메서드로 개별 클래스를 조작할 때 유용
+
+// console.log( first.className = "hola"); // ? hola
+// <span class="hola">hello</span>
+
+// first.classList.add("hello");
+// <span class="first hello">hello</span>
+
+// first.classList.remove("hello");
+// <span class="first">hello</span>
+
+// first.classList.toggle("hello");
+// <span class="first hello">hello</span>
+
+// console.log(first.classList.contains("is-active")); // ? false
+
+
+
+
+// lib/dom/css.js 참고
+
+addClass(".first", "hello");
+// <span class="first hello">hello</span>
+
+removeClass(".first", "hello");
+// <span class="first">hello</span>
+
+// toggleClass(".first", "hello");
+// <span class="first hello">hello</span>
+
+
+
+
+/* 스타일 변경 방법 --------------------------------------------------------- */
+
+// - style.cssText - "style" 속성 전체에 대응하므로 스타일 전체에 대한 문자열 저장
+
+
+/* 계산된 스타일 읽기 ------------------------------------------------------- */
+
+// - getComputedStyle(element, [pseudoElement]) `읽기 전용`
---
+++ core_javascript/client/lib/dom/css.js
@@ -0,0 +1,37 @@
+// addClass
+function addClass(node, className){
+  if(typeof node == "string"){
+    node = getNode(node);
+  }
+  if(typeof className !== "string"){
+    TypeError("addClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
+  }
+  node.classList.add(className);
+}
+
+// removeClass
+function removeClass(node, className){
+  if(typeof node == "string"){
+    node = getNode(node);
+  }
+  if(!className){
+    node.className = "";
+    return;
+  }
+  if(typeof className !== "string"){
+    TypeError("removeClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
+  }
+  node.classList.remove(className);
+  // 변경하기 : 대상의 클래스를 지운다 => set만 하기 때문에 return 할 필요 없음
+}
+
+// toggleClass
+function toggleClass(node, className){
+  if(typeof node == "string"){
+    node = getNode(node);
+  }
+  if(typeof className !== "string"){
+    TypeError("toggleClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
+  }
+  node.classList.toggle(className)
+}
