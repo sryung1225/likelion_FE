3d9763e13efb5df29cd65422936e24cdb7d4ba07
sryung <sryung1225@gmail.com>
2023-01-11 17:18:14 +0900

    [Day31] 오타수정

--- core_javascript/client/lib/dom/bindEvent.js
+++ core_javascript/client/lib/dom/bindEvent.js
@@ -6,5 +6,5 @@ function bindEvent(node, type, handler){
     typeError('bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다.')
   }
   node.addEventListener(type, handler);
-  return () => node.remobeEventListener(type, handler); // 클로저
+  return () => node.removeEventListener(type, handler); // 클로저
 }
