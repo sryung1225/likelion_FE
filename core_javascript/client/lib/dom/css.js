6f5ba8a0a1f491de05ca2c7dd65e6282d0a30a42
sryung <sryung1225@gmail.com>
2023-01-11 11:18:39 +0900

    [Day31] css.js 오타 수정

--- core_javascript/client/lib/dom/css.js
+++ core_javascript/client/lib/dom/css.js
@@ -19,7 +19,7 @@ function removeClass(node, className){
     return;
   }
   if(typeof className !== "string"){
-    TypeError("removeClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
+    typeError("removeClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
   }
   node.classList.remove(className);
   // 변경하기 : 대상의 클래스를 지운다 => set만 하기 때문에 return 할 필요 없음
@@ -31,7 +31,7 @@ function toggleClass(node, className){
     node = getNode(node);
   }
   if(typeof className !== "string"){
-    TypeError("toggleClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
+    typeError("toggleClass 함수의 두 번째 인자는 문자 타입이여야 합니다.");
   }
   node.classList.toggle(className)
 }
