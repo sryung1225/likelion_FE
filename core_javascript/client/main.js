900af968bafba480a7910ff89bffbbaaa7915b23
sryung <sryung1225@gmail.com>
2023-01-16 14:05:14 +0900

    [Day34] 랜덤 다이스 실습 : 레코드 리스트 만들기

--- core_javascript/client/main.js
+++ core_javascript/client/main.js
@@ -1,31 +1,50 @@
 
-import { diceAnimation, getNode } from "./lib/index.js";
+import { diceAnimation,
+        disableElement,
+        enableElement,
+        getNode,
+        getNodes,
+        invisibleElement,
+        visibleElement } from "./lib/index.js";
 
-// [ 주사위 굴리기 ]
-// 1. dice 애니메이션 불러오기
-// 2. bindEvent 유틸함수 만들기
-// 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
-// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
+// [ 레코드 리스트 보이기 ]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기
 
-// diceAnimation()
-
-const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes(".buttonGroup > button");
+const recordListWrapper = getNode(".recordListWrapper");
 
 // IIFE 패턴 적용
-const handlerRollingDice = (() => {
+const handleRollingDice = (() => {
   let isRolling = false;
   let stopAnimation;
   return () => {
     if(!isRolling){
       // console.log("첫번째 클릭");
       stopAnimation = setInterval(diceAnimation, 100);
+      disableElement(recordButton);
+      disableElement(resetButton);
     } else {
       // console.log("두번째 클릭");
       clearInterval(stopAnimation);
+      enableElement(recordButton);
+      enableElement(resetButton);
     }
     isRolling = !isRolling;
   }
 })();
+const handleRecord = () => {
+  visibleElement(recordListWrapper);
+}
+const handleReset = () => {
+  invisibleElement(recordListWrapper);
+}
 
-rollingDiceButton.addEventListener("click", handlerRollingDice);
+rollingDiceButton.addEventListener("click", handleRollingDice);
+recordButton.addEventListener("click", handleRecord);
+resetButton.addEventListener("click", handleReset);
 // let eventOff = bindEvent(rollingDiceButton, "click", handlerRollingDice);
