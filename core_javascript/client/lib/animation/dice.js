8a7652318d7829870928ee70d033e970a776b19d
sryung <sryung1225@gmail.com>
2023-01-17 09:15:32 +0900

    [Day34] 랜덤 다이스 실습 : 메모이제이션

--- core_javascript/client/lib/animation/dice.js
+++ core_javascript/client/lib/animation/dice.js
@@ -2,8 +2,9 @@
 
 import { attr } from "../dom/attr.js";
 import { getNode } from '../dom/getNode.js'
+import { memo } from "../utils/memo.js"
 
-const cube = getNode('#cube');
+memo("cube", () => getNode("#cube"));
 
 let random;
 gsap.to(cube,{duration:1,rotationX:100,rotationY:-100,ease:'back(10)'});
