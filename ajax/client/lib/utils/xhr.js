8518522d69a9f9b00ef29cdef9cbf1fbd101b079
sryung <sryung1225@gmail.com>
2023-01-17 15:17:03 +0900

    [Day35] Ajax : 비동기통신 확인하기 - 함수로 만들기 + 구조분해할당

--- ajax/client/lib/utils/xhr.js
+++ ajax/client/lib/utils/xhr.js
@@ -7,22 +7,84 @@
   4:complete // 완료
 */
 
-const xhr = new XMLHttpRequest();
-// console.log(xhr.readyState); // ? 0
 
-// 비동기 통신 오픈
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-// console.log(xhr.readyState); // ? 1
-
-xhr.addEventListener("readystatechange", () => {
-  if(xhr.status >= 200 && xhr.status < 400){
-    if(xhr.readyState === 4){
-      console.log("통신 성공");
-    }
-  } else {
-    console.log("통신 실패");
+// xhrData 함수 만들기 method, url
+function xhrData({
+  url = "",
+  method = "GET",
+  body = null,
+  onSuccess = null,
+  headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   }
-})
+} = {}){
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  // console.log(xhr); // ? XMLHttpRequest {onreadystatechange: null, readyState: 1, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}
 
-// 서버에 요청
-xhr.send();
+  // headers에 여러가지 넣을 경우를 대비하는 것... 이지만 오류 날까봐 나중에 이어하기로해서 주석처리
+  // Object.entries(headers).forEach(([key, value]) => {
+  //   xhr.setRequestHeader(key, value);
+  // });
+
+  xhr.addEventListener("readystatechange", () => {
+    const {status, readyState, response} = xhr; // 객체 구조 분해 할당
+    if(status >= 200 && status < 400){
+      if(readyState === 4){
+        console.log("통신 성공");
+
+        onSuccess(JSON.parse(response));
+        console.log();
+      }
+    } else {
+      onFail("통신 실패");
+    }
+  })
+  xhr.send(JSON.stringify(body));
+}
+
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail
+  })
+}
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: "POST",
+    url,
+    body,
+    onSuccess,
+    onFail
+  })
+}
+xhrData.put = (body, url, onSuccess, onFail) => {
+  xhrData({
+    method: "PUT",
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+xhrData.delete = (body, url, onSuccess, onFail) => {
+  xhrData({
+    method: "DELETE",
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+}
+
+// xhrData.get(
+//   "https://jsonplaceholder.typicode.com/users",
+//   (result) => {
+//     console.log(result);
+//   },
+//   (err) => {
+//     console.error(err);
+//   }
+// )
