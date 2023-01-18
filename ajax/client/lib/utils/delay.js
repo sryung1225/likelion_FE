fe3ccb7194889cc7f926aeeafdd8a7d416ab9116
sryung <sryung1225@gmail.com>
2023-01-18 10:24:17 +0900

    [Day36] 비동기 통신 : Promise

--- ajax/client/lib/utils/delay.js
+++ ajax/client/lib/utils/delay.js
@@ -7,6 +7,7 @@ function delay(callback, timeout = 1000){
   setTimeout(callback, timeout);
 }
 
+// 콜백지옥
 delay(()=>{
   first.style.top = "-100px";
   delay(()=>{
@@ -16,3 +17,33 @@ delay(()=>{
     })
   })
 })
+
+// 프라미스
+function delayP(timeout = 1000){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("성공!");
+      // reject("실패!");
+    }, timeout);  // ? Promise {<pending>}
+  })
+}
+// console.log(delayP()); // ? Promise {<pending>}
+// delayP().then(() => {});
+delayP()
+.then((res) => {
+  console.log(res);
+})
+
+// 위 콜백지옥을 프라미스를 이용해 개선할 수 있음
+delayP()
+.then(() => {
+  first.style.top = "-100px";
+  return delayP();
+})
+.then(() => {
+  first.style.transform = "rotate(360deg)";
+  return delayP();
+})
+.then(() => {
+  first.style.top = "0px";
+});
