8a42f023f7a534dbaa8e324868bcf98e49b8a392
sryung <sryung1225@gmail.com>
2023-02-14 09:08:41 +0900

    [Day52] 유사하게 구현한 가상 DOM 사용

--- REACT/01-react-browser/frontend/src/main.js
+++ REACT/01-react-browser/frontend/src/main.js
@@ -1,5 +1,16 @@
 import './utils/supportJavaScript.js';
 // import './vanilla.script.js';
-import './react.script.js';
+// import './react.script.js';
 
 // console.log('React 개발 시작하기! 😊');
+
+
+import { createElement as h, render } from './likeReact/index.js';
+
+const app = h('div', {
+  className: 'app'
+}, h('h1', null, 'Hello Virtual DOM'));
+
+render(app, document.getElementById('root'));
+
+console.log(app);
