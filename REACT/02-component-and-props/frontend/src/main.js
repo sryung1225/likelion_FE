13385604c71d4693297b8bdae5da4247d599be6c
sryung <sryung1225@gmail.com>
2023-02-14 13:52:13 +0900

    [Day53] React 컴포넌트 vs. React 엘리먼트

--- REACT/02-component-and-props/frontend/src/main.js
+++ REACT/02-component-and-props/frontend/src/main.js
@@ -7,7 +7,33 @@ import { Group } from './learn/react.components.js';
 
 // 사용법 1
 // 웹브라우저가 인식 가능한 HTML 표준 엘리먼트 이름 (문자 값)
-console.log(React.createElement('details'));
+// console.log(React.createElement('details'));
 // 사용법 2
-// 컴포넌트 함수 또는 클래스 참조 전달 
-console.log(React.createElement(Group));
+// 컴포넌트 함수 또는 클래스 참조 전달
+// console.log(React.createElement(Group));
+
+
+// ReactDOM이 하는 일은 React가 한 결과(가상 DOM 생성)를 실제 DOM에 렌더링(마운트) 하는 것
+// ReactDOM의 절차
+// 1. ReactDOM의 루트를 생성한다. (실제 DOM 요소를 전달)
+// ReactDOM.createRoot(document.getElementById('root'));
+// 2. 생성된 ReactDOM의 루트 요소(객체)를 통해 실제 DOM에 렌더링 한다.
+// reactDomRoot.render();
+
+const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
+
+reactDomRoot.render(
+  // React 컴포넌트 -> React 엘리먼트
+  
+  // 한 번 작성하는 용
+  // React.createElement(Group)
+  // 리액트 월드에서 root는 단 한개만 존재해야 하기 때문에, 여러번 작성하기 위해서는 [] 로 묶어줘서 사용해야 함
+
+  // 여러번 작성하는 용
+  [
+    React.createElement(Group),
+    React.createElement(Group),
+    React.createElement(Group),
+    React.createElement(Group)
+  ]
+);
