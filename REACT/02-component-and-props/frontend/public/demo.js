d63ff27e3519ee1b4a9ef58ce0216290807e25c7
sryung <sryung1225@gmail.com>
2023-02-15 09:23:48 +0900

    [Day53] React 컴포넌트 추출(분리) - 관심사의 분리 - 효율적인 재사용 - 효과적인 유지 보수

--- REACT/02-component-and-props/frontend/public/demo.js
+++ REACT/02-component-and-props/frontend/public/demo.js
@@ -1,52 +1,117 @@
-/* React 컴포넌트 --------------------------------------------------------------- */
+/* React 컴포넌트 -------------------------------------------------------------- */
 
-// 함수(형) 컴포넌트
-function Group(props) {
-  console.log(props.lang);
-  console.log(props.content);
+// 일반함수 이름 : camelCase
+// 컴포넌트 이름 : PascalCase
 
-  // JSX 보간법(interpolation)
-  // <element prop={value}>this is {content}</element>
+/* 함수 컴포넌트 ------------------------------------------------------------------ */
 
+function AppLogo() {
   return (
-    <div role="group" lang={props.lang}>
-      {props.content}
-    </div>
+    <h1>
+      <a href="https://netflix.com/">
+        <img src="" alt="" /> 넷플릭스
+      </a>
+    </h1>
   )
 }
 
-// 클래스 컴포넌트
-class Logo extends React.Component {
-  // 생성자
-  // constructor(props) {
-  //   super(props);
-  // }
+function AppSearch() {
+  return (
+    <form role="search">
+      <label htmlFor="search">검색</label>
+      <input id="search" type="search" />
+    </form>
+  )
+}
 
-  // 렌더링
-  render() {
-    // React는 99% JavaScript
-    // 구조 분해 할당
-    const { path, label } = this.props;
+function AppNav() {
+  return (
+    <nav aria-label="">
+      <ul>
+        <li>
+          <a href="">link 1</a>
+        </li>
+        <li>
+          <a href="">link 2</a>
+        </li>
+        <li>
+          <a href="">link 3</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
-    // React 엘리먼트 반환
-    return (
-      <img src={path} alt={label} />
-    );
+function AppHeader() {
+  return (
+    <header>
+      <AppLogo />
+      <AppSearch />
+      <AppNav />
+    </header>
+  )
+}
+
+function AppMain() {
+  return (
+    <main>
+      <AppLogo />
+      <p>앱 메인 정보를 입력합니다.</p>
+    </main>
+  )
+}
+
+function AppFooter() {
+  return (
+    <footer>
+      <AppLogo />
+      <address>
+        <small>저작권 정보</small>
+      </address>
+    </footer>
+  )
+}
+
+/* 클래스 컴포넌트 ----------------------------------------------------------------- */
+// - 2018년까지는....
+// - 클래스 컴포넌트는 상태를 가질 수 있다.
+// - 함수 컴포넌트는 상태를 가질 수 없다.
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // 컴포넌트 상태 
+    // 애플리케이션의 상태
+    // 상태 업데이트 → 다시 렌더링!!!
+    // 선언형 프로그래밍
+    this.state = {
+      headline: '',
+      description: '',
+      subjects: []
+    }
   }
 
+  render() {
+    // const {  } = this.props;
 
-  /* React DOM 렌더 ------------------------------------------------------------- */
+    return (
+      <div className="app">
+        <AppHeader />
+        <AppMain />
+        <AppFooter />
+      </div>
+    )
+  }
+}
 
-  const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
+/* React DOM 렌더 ------------------------------------------------------------- */
+
+const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
 
 reactDomRoot.render(
-  <React.Fragment>
-    <Logo path="/assets/react-logo.svg" label="React" />
-    <Group />
-    <Logo
-      path="https://upload.vectorlogo.zone/logos/nextjs/images/60eff509-53dd-4280-92e7-7318fa02e934.svg"
-      label="Next.js"
-      unknown="아무 것도 몰라요~"
-    />
-  </React.Fragment >
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
