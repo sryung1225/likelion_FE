deee8032d7b4ecd5088bfa5fd1d75a4ac00578fd
sryung <sryung1225@gmail.com>
2023-02-17 23:20:02 +0900

    [Day55-*] React 기초 특강 - 기본 셋팅 : create-react-app

---
+++ REACT_basic/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React 펀더멘탈</h1>
+      <p>React 1주차 학습 내용을 다시 한 번 정리합니다.</p>
+    </div>
+  );
+}
