2223de43446b0bba70fa6c088c1b1d578d261066
sryung <sryung1225@gmail.com>
2023-02-24 16:46:27 +0900

    [Day54] 클래스의 멤버 변수 활용

--- REACT/03-jsx-in-depth/frontend/src/components/App.js
+++ REACT/03-jsx-in-depth/frontend/src/components/App.js
@@ -1,21 +1,23 @@
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // 2022 6. 표준 기술 : public, private 클래스 필드, 피처
 
-    this.state = {
-      headline: 'React Application',
-    };
+  state = {
+    headline: 'React Application'
+  };
 
-    this.handleChangeHeadline = this.handleChangeHeadline.bind(this);
-  }
+  // 클래스에서는 이렇게 기억할 수 있는 임의의 데이터를 관리할 수 있다
+  // 함수는? 못한다 => React Hooks 사용 (useRef)
+  // 메모이제이션 패턴
+  originalHeadline = this.state.headline;
 
-  handleChangeHeadline() {
+  handleChangeHeadline = () => {
     this.setState({
-      headline: 'NEW HEADLINE! 😃',
+      headline: 'NEW HEADLINE! 😃'
     });
   }
 
   render() {
+    console.log(this);
     return (
       <div data-component="App">
         <h1>{this.state.headline}</h1>
