6af42eddea517f3d962e47a594a9f2c429e94930
sryung <sryung1225@gmail.com>
2023-02-25 22:39:35 +0900

    [Day60-*] typescript 번역기(tsconfig.json) 설정

--- REACT_special/hello-ts/index.js
+++ REACT_special/hello-ts/index.js
@@ -1,12 +1,26 @@
-let 이름; // 문자열로 자료타입 지정
-// 이름 = 0 // 에러!
-이름 = '0'; // => 이름 = '0';
-let 랄라; // 문자열 또는 숫자
-랄라 = '1';
-let 유저;
-let 유저2;
-let 유저3;
-// 유저3 = {
-//   아이디: '1',
-//   아바타: '2'
-// } // 에러!
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    exports.카운터 = void 0;
+    var 카운터 = /** @class */ (function () {
+        function 카운터(값) {
+            this.값 = 값;
+        }
+        카운터.prototype.더하기 = function () {
+            this.값 += 1;
+        };
+        카운터.prototype.빼기 = function () {
+            this.값 -= 1;
+        };
+        return 카운터;
+    }());
+    exports.카운터 = 카운터;
+});
--- REACT_special/hello-ts/index.ts
+++ REACT_special/hello-ts/index.ts
@@ -1,34 +1,15 @@
-let 이름: string // 문자열로 자료타입 지정
-// 이름 = 0 // 에러!
-이름 = '0' // => 이름 = '0';
+export class 카운터 {
+  private 값
 
-let 랄라: string | number // 문자열 또는 숫자
-랄라 = '1'
+  constructor(값: number) {
+    this.값 = 값
+  }
 
-type 유저타입 = { 아이디: string, 아바타: string }
-let 유저: 유저타입
-// 유저 = '' // 에러!
+  더하기() {
+    this.값 += 1
+  }
 
-type 사람타입 = { 이름: string }
-type 유저타입2 = { 아이디: string, 아바타: string }
-
-let 유저2: 사람타입 & 유저타입2
-// 유저2 = {
-//   아이디: '1',
-//   아바타: '2'
-// } // 에러!
-
-interface 사람타입2 {
-  이름: string
+  빼기() {
+    this.값 -= 1
+  }
 }
-
-interface 유저타입3 extends 사람타입2 {
-  아이디: string,
-  아바타: string
-}
-
-let 유저3: 유저타입3
-// 유저3 = {
-//   아이디: '1',
-//   아바타: '2'
-// } // 에러!
