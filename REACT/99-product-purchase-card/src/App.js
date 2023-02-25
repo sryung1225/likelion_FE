f88ef1bb86ca0ef980a1be4bc3b98af378299128
sryung <sryung1225@gmail.com>
2023-02-26 06:08:06 +0900

    [Day60-*] Counter 구현 - App에서 만든 count, setCount props를 Counter에 전달 - Counter는 받은 state를 사용 - 기본 수량 1, 0~10개 설정

--- REACT/99-product-purchase-card/src/App.js
+++ REACT/99-product-purchase-card/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Product from './components/Product';
 import Counter from './components/Counter';
@@ -6,10 +6,11 @@ import Sum from './components/Sum';
 import PurchaseBtn from './components/PurchaseBtn';
 
 export default function App() {
+  const [count, setCount] = useState(1);
   return (
     <div className="product-purchange-card">
       <Product />
-      <Counter />
+      <Counter count={count} setCount={setCount} />
       <Sum />
       <PurchaseBtn />
     </div>
--- REACT/99-product-purchase-card/src/components/Counter.js
+++ REACT/99-product-purchase-card/src/components/Counter.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Counter = () => {
+const Counter = ({ count, setCount }) => {
+  const onIncrease = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+  const onDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="product__count">
         <div className="counter">
-          <button className="counter--btn" type="button" aria-label="상품 수량 1 빼기">-</button>
-          <span className="counter--text" aria-label="상품 수량">1</span>
-          <button className="counter--btn" type="button" aria-label="상품 수량 1 더하기">+</button>
+          <button
+            className="counter--btn" type="button" aria-label="상품 수량 1 빼기"
+            onClick={onDecrease}
+          >-</button>
+          <span className="counter--text" aria-label="상품 수량">
+            {count}
+          </span>
+          <button
+            className="counter--btn" type="button" aria-label="상품 수량 1 더하기"
+            onClick={onIncrease}
+          >+</button>
         </div>
       </div>
     </React.Fragment>
