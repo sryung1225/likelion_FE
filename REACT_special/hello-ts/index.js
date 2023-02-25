a24207d13041faed91aea47d65fc842be1e9817c
sryung <sryung1225@gmail.com>
2023-02-25 22:50:23 +0900

    [Day60-*] typescript 문법 : narrowing / assertion / generic

--- REACT_special/hello-ts/index.js
+++ REACT_special/hello-ts/index.js
@@ -1,26 +1,9 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    exports.__esModule = true;
-    exports.카운터 = void 0;
-    var 카운터 = /** @class */ (function () {
-        function 카운터(값) {
-            this.값 = 값;
-        }
-        카운터.prototype.더하기 = function () {
-            this.값 += 1;
-        };
-        카운터.prototype.빼기 = function () {
-            this.값 -= 1;
-        };
-        return 카운터;
-    }());
-    exports.카운터 = 카운터;
-});
+"use strict";
+var 루트엘리먼트 = document.getElementById('root');
+루트엘리먼트.innerHTML = '리엑트 렌더링!';
+function 함수(파라미터) {
+    alert(파라미터);
+}
+함수(1);
+함수('1');
+함수(true);
--- REACT_special/hello-ts/index.ts
+++ REACT_special/hello-ts/index.ts
@@ -1,15 +1,28 @@
-export class 카운터 {
-  private 값
+/* -- Narrowing : 타입 좁혀가기 -- */
+// const 함수 = (프로퍼티가_다른_객체: { a: number } | { b: number }) => {
+//   if ('a' in 프로퍼티가_다른_객체) {
+//     return 프로퍼티가_다른_객체.a
+//   }
 
-  constructor(값: number) {
-    this.값 = 값
-  }
+//   return 프로퍼티가_다른_객체.b
+// }
 
-  더하기() {
-    this.값 += 1
-  }
+// const 루트엘리먼트 = document.getElementById('root')
+// if (루트엘리먼트) {
+//   루트엘리먼트.innerHTML = '리엑트 렌더링!'
+// }
 
-  빼기() {
-    this.값 -= 1
-  }
+
+/* -- Assertion : 타입 단정짓기 -- */
+const 루트엘리먼트 = document.getElementById('root') as HTMLDivElement
+루트엘리먼트.innerHTML = '리엑트 렌더링!'
+
+
+/* -- 제네릭 : 로직 재활용 -- */
+function 함수<실행할때_정해지는_타입>(파라미터: 실행할때_정해지는_타입) {
+  alert(파라미터)
 }
+
+함수(1)
+함수('1')
+함수(true)
