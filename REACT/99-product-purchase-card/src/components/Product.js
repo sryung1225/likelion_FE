dd8393055a35aaef820b907309db2d348d113f2d
sryung <sryung1225@gmail.com>
2023-02-26 12:58:02 +0900

    [Day60-*] 할인가/정가/할인율 접근성 코드 수정

--- REACT/99-product-purchase-card/src/components/Product.js
+++ REACT/99-product-purchase-card/src/components/Product.js
@@ -6,10 +6,10 @@ const Product = () => {
       <img className="product__image" src="assets/productImage.jpg" alt="래피젠 코로나 자가 검사 키트" />
       <h2 className="product__title">래피젠 코로나 자가 검사 키트 2개입X1박스</h2>
       <div className="product__price">
-        <b className="sale-rate" aria-label="할인율">88%</b>
+        <b className="sale-rate">88%</b>
         <span className="price">
-          <del className="price--original" aria-label="정가">56,760원</del>
-          <span className="price--sale" aria-label="할인가">6,370원</span>
+          <del className="price--original">56,760원</del>
+          <span className="price--sale">6,370원</span>
         </span>
       </div>
       <div className="product__delivery">
