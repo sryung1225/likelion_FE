92df3d137b8fe10001310cc1598b9f166ad7b0d4
sryung <sryung1225@gmail.com>
2023-03-07 15:57:08 +0900

    [Day65] Recoil 훅의 유형별 사용법 - useRecoilState (읽기/쓰기) - 각각 쪼개서 관리하기 위해 분리 - 읽기가 포함된 카운트만 렌더링되고, 쓰기 전용인 버튼은 더 이상 렌더링되지 않음을 확인

--- REACT/12-recoil/learning-recoil/src/RecoilCounter/Count.jsx
+++ REACT/12-recoil/learning-recoil/src/RecoilCounter/Count.jsx
@@ -1,7 +1,13 @@
-export default function Count({ children }) {
+import { useRecoilValue } from 'recoil';
+import { countState } from '@/@store/countState';
+
+export default function Count() {
+
+  const count = useRecoilValue(countState); // 읽기(GET)
+
   return (
     <output>
-      {children}
+      {count}
     </output>
   );
 }
--- REACT/12-recoil/learning-recoil/src/RecoilCounter/Counter.jsx
+++ REACT/12-recoil/learning-recoil/src/RecoilCounter/Counter.jsx
@@ -2,25 +2,12 @@ import Count from './Count';
 import CountButton from './CountButton';
 import classes from './Counter.module.css';
 
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { countState } from '@/@store/countState';
-
 export default function Counter() {
-  // 읽기(GET)/쓰기(SET) useRecoilState
-  // 읽기(GET)           useRecoilValue
-  // 쓰기(SET)           useSetRecoilState
-
-  const count = useRecoilValue(countState); // 읽기(GET)
-  const setCount = useSetRecoilState(countState); // 쓰기(SET)
 
   return (
     <div className={classes.RecoilCounter}>
-      <Count>{count}</Count>
-      <CountButton
-        onClick={() => setCount((count) => count + 1)}
-      >
-        리코일 카운트
-      </CountButton>
+      <Count />
+      <CountButton>리코일 카운트</CountButton>
     </div>
   )
 }
