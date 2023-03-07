68f489b5c6f6304313f3171fa423d38d3b2ae910
sryung <sryung1225@gmail.com>
2023-03-07 15:02:55 +0900

    [Day65] Vite 절대 경로 구성

--- REACT/12-recoil/learning-recoil/vite.config.js
+++ REACT/12-recoil/learning-recoil/vite.config.js
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: resolve(__dirname, './src'),
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
   },
 });
