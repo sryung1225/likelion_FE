3be11a7fa8b0f070e0b91752729aea9d4ea36ee9
sryung <sryung1225@gmail.com>
2023-03-08 10:50:43 +0900

    [Day66] ExchangePage - Selector (GET) - 편의상 TimeTravelObserver 비활성화 - ExchangePage View 생성

--- REACT/12-recoil/learning-recoil/src/App.jsx
+++ REACT/12-recoil/learning-recoil/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
-import AppFooter from './AppFooter';
-import AppHeader from './AppHeader';
-import PlayList, { RestPlayListButton } from './PlayList';
+import ExchangePage from './ExchangePage';
 
 
 function App() {
   return (
     <div className="App">
-      <AppHeader />
-      <PlayList />
-      <AppFooter />
-      <RestPlayListButton />
+      <ExchangePage />
     </div>
   );
 }
--- REACT/12-recoil/learning-recoil/src/main.jsx
+++ REACT/12-recoil/learning-recoil/src/main.jsx
@@ -2,7 +2,7 @@
 import { RecoilRoot } from 'recoil';
 import ReactDOM from 'react-dom/client';
 import { RecoilLogger } from 'recoil-devtools-logger';
-import { TimeTravelObserver } from './@recoilDebug';
+// import { TimeTravelObserver } from './@recoilDebug';
 import App from './App';
 import './index.css';
 
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RecoilRoot>
       <App />
       <RecoilLogger />
-      <TimeTravelObserver visible shortcut="u" />
+      {/* <TimeTravelObserver visible shortcut="u" /> */}
     </RecoilRoot>
   </>
 );
